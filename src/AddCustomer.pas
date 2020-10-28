75e035b2d324990cce07af3226965cbf5fcc5f28
mojtaba <mojtabakarimi1997@gmail.com>
2020-10-28 23:37:44 +0330

    modified DM

---
+++ src/AddCustomer.pas
@@ -0,0 +1,24 @@
+unit AddCustomer;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfrmAddCustomer = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmAddCustomer: TfrmAddCustomer;
+
+implementation
+
+{$R *.dfm}
+
+end.
--- src/Main.pas
+++ src/Main.pas
@@ -3,20 +3,20 @@ unit Main;
 interface
 
 uses
-  DataAccessLayer, Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  AddCustomer, DataAccessLayer, Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.ExtCtrls,
-  Vcl.StdCtrls, Data.DB, Vcl.Grids, Vcl.DBGrids, Vcl.DBCtrls;
+  Vcl.StdCtrls, Data.DB, Vcl.Grids, Vcl.DBGrids, Vcl.DBCtrls,
+  Vcl.Imaging.pngimage;
 
 type
   TfrmMain = class(TForm)
     StatusBarFooter: TStatusBar;
     GBLeftMenu: TGroupBox;
-    DBGFoods: TDBGrid;
-    DBNavigator1: TDBNavigator;
-    DBLCBOrderCustomer: TDBLookupComboBox;
+    imgAddCustomer: TImage;
     procedure status_bar_panels_divider;
     procedure FormCreate(Sender: TObject);
+    procedure imgAddCustomerClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -33,6 +33,13 @@ implementation
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   status_bar_panels_divider;
+end;
+
+procedure TfrmMain.imgAddCustomerClick(Sender: TObject);
+begin
+  Application.CreateForm(TfrmAddCustomer, frmAddCustomer);
+  frmAddCustomer.ShowModal;
+  frmAddCustomer.Destroy;
 end;
 
 procedure TfrmMain.status_bar_panels_divider;
